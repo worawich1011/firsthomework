cf89689c6a0299815199801a8b21351201813442
worawich1011 <68009840+worawich1011@users.noreply.github.com>
2020-07-08 19:00:26 +0700

    Add files via upload

---
+++ homework.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	cout<<"    #    "<<endl;
+	cout<<"   # #   "<<endl;
+	cout<<"  #   #  "<<endl;
+	cout<<" #     # "<<endl;
+	cout<<" ####### "<<endl;
+	cout<<" #     # "<<endl;
+	cout<<endl;
+	cout<<" Worawich Piriyayothakul "<<endl;
+	cout<<endl;
+}
